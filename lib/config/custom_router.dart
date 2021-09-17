db3beef6a2030ae2506568aabeb54b251ad5d0f4
sanasif786ka <asif786ka@gmail.com>
2021-09-17 15:51:43 +0200

    Added custom navigation router and splash screen.

---
+++ lib/config/custom_router.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_twitter_glsample/screens/screens.dart';
+
+class CustomRouter {
+  static Route onGenerateRoute(RouteSettings settings) {
+    switch (settings.name) {
+      case '/':
+        return MaterialPageRoute(
+          settings: const RouteSettings(name: 'name'),
+          builder: (_) => const Scaffold(),
+        );
+      case SplashScreen.routeName:
+        return SplashScreen.route();
+      default:
+        return _errorRoute();
+    }
+  }
+
+  static Route _errorRoute() {
+    return MaterialPageRoute(
+      settings: const RouteSettings(name: '/error'),
+      builder: (_) => Scaffold(
+        appBar: AppBar(title: const Text('Error')),
+        body: const Center(child: Text('Something went wrong')),
+      ),
+    );
+  }
+}
