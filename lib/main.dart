424f6465e0b7d0a0b12d753fd908a46b61b0f629
sanasif786ka <asif786ka@gmail.com>
2021-09-17 18:55:30 +0200

    Splash screen routing depending upon state.

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,17 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_twitter_glsample/blocs/simple_bloc_observer.dart';
 import 'package:flutter_twitter_glsample/config/custom_router.dart';
+import 'package:flutter_twitter_glsample/repositories/auth/auth_repository.dart';
 import 'package:flutter_twitter_glsample/screens/screens.dart';
+
+import 'blocs/auth/auth_bloc.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  Bloc.observer = SimpleBlocObserver();
   runApp(MyApp());
 }
 
@@ -13,36 +19,53 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Twitter GLSample Asif',
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-        scaffoldBackgroundColor: Colors.grey[50],
-        appBarTheme: AppBarTheme(
-            brightness: Brightness.light,
-            color: Colors.white,
-            iconTheme: const IconThemeData(color: Colors.black)),
-        textTheme: const TextTheme(
-          headline6: TextStyle(
-              color: Colors.black, fontSize: 20.0, fontWeight: FontWeight.w600),
+    return MultiRepositoryProvider(
+      providers: [
+        RepositoryProvider<AuthRepository>(
+          create: (_) => AuthRepository(),
+        )
+      ],
+      child: MultiBlocProvider(
+        providers: [
+          BlocProvider<AuthBloc>(
+            create: (context) =>
+                AuthBloc(authRepository: context.read<AuthRepository>()),
+          )
+        ],
+        child: MaterialApp(
+          title: 'Flutter Twitter GLSample Asif',
+          debugShowCheckedModeBanner: false,
+          theme: ThemeData(
+            // This is the theme of your application.
+            //
+            // Try running your application with "flutter run". You'll see the
+            // application has a blue toolbar. Then, without quitting the app, try
+            // changing the primarySwatch below to Colors.green and then invoke
+            // "hot reload" (press "r" in the console where you ran "flutter run",
+            // or simply save your changes to "hot reload" in a Flutter IDE).
+            // Notice that the counter didn't reset back to zero; the application
+            // is not restarted.
+            primarySwatch: Colors.blue,
+            scaffoldBackgroundColor: Colors.grey[50],
+            appBarTheme: AppBarTheme(
+                brightness: Brightness.light,
+                color: Colors.white,
+                iconTheme: const IconThemeData(color: Colors.black)),
+            textTheme: const TextTheme(
+              headline6: TextStyle(
+                  color: Colors.black,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.w600),
+            ),
+            // This makes the visual density adapt to the platform that you run
+            // the app on. For desktop platforms, the controls will be smaller and
+            // closer together (more dense) than on mobile platforms.
+            visualDensity: VisualDensity.adaptivePlatformDensity,
+          ),
+          onGenerateRoute: CustomRouter.onGenerateRoute,
+          initialRoute: SplashScreen.routeName,
         ),
-        // This makes the visual density adapt to the platform that you run
-        // the app on. For desktop platforms, the controls will be smaller and
-        // closer together (more dense) than on mobile platforms.
-        visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      onGenerateRoute: CustomRouter.onGenerateRoute,
-      initialRoute: SplashScreen.routeName,
     );
   }
 }
--- lib/screens/splash/splash_screen.dart
+++ lib/screens/splash/splash_screen.dart
@@ -1,22 +1,42 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_twitter_glsample/blocs/auth/auth_bloc.dart';
+import 'package:flutter_twitter_glsample/screens/login/login_screen.dart';
+import 'package:flutter_twitter_glsample/screens/nav/nav_screen.dart';
 
-class  SplashScreen extends StatelessWidget {
 
+class SplashScreen extends StatelessWidget {
   static const String routeName = '/splash';
 
   static Route route() {
     return MaterialPageRoute(
-        settings:const RouteSettings(name: routeName),
-        builder: (_) => SplashScreen()
+      settings: const RouteSettings(name: routeName),
+      builder: (_) => SplashScreen(),
     );
   }
+
   @override
   Widget build(BuildContext context) {
     return WillPopScope(
       onWillPop: () async => false,
-      child: const Scaffold(
-        body: Center(
-          child: CircularProgressIndicator(),
+      child: BlocListener<AuthBloc, AuthState>(
+        listenWhen: (prevState, state) =>
+        prevState.status !=
+            state
+                .status, // Prevent listener from triggering if status did not change
+        listener: (context, state) {
+          if (state.status == AuthStatus.unauthenticated) {
+            // Go to the Login Screen.
+            Navigator.of(context).pushNamed(LoginScreen.routeName);
+          } else if (state.status == AuthStatus.authenticated) {
+            // Go to the Nav Screen.
+            Navigator.of(context).pushNamed(NavScreen.routeName);
+          }
+        },
+        child: const Scaffold(
+          body: Center(
+            child: CircularProgressIndicator(),
+          ),
         ),
       ),
     );
