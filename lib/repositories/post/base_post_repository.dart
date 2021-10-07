962c4e014c6ddc3306c57cd6e902948c3fdb086a
sanasif786ka <asif786ka@gmail.com>
2021-10-07 20:59:59 +0200

    Added liked posts cubit.

--- lib/repositories/post/base_post_repository.dart
+++ lib/repositories/post/base_post_repository.dart
@@ -4,7 +4,10 @@ import 'package:flutter_twitter_glsample/models/post_model.dart';
 abstract class BasePostRepository {
   Future<void> createPost({Post post});
   Future<void> createComment({Comment comment});
+  void createLike({Post post, String userId});
   Stream<List<Future<Post>>> getUserPosts({String userId});
   Stream<List<Future<Comment>>> getPostComments({String postId});
   Future<List<Post>> getUserFeed({String userId, String lastPostId});
+  Future<Set<String>> getLikedPostIds({String userId, List<Post> posts});
+  void deleteLike({String postId, String userId});
 }
